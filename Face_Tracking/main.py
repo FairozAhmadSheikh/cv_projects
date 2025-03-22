8ddb4983b574b4e8f4c4b68891364125d55f874b
FairozAhmadSheikh <fairozahmadsheikh@gmail.com>
2025-03-22 09:39:30 +0530

    face

--- Face_Tracking/main.py
+++ Face_Tracking/main.py
@@ -4,15 +4,13 @@ def main():
     # Load the pre-trained Haar cascade for face detection
     face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
     
-    # Start video capture
-    cap = cv2.VideoCapture(0)  # 0 for default webcam
+    cap = cv2.VideoCapture(0)  
     
     while True:
         ret, frame = cap.read()
         if not ret:
             break
         
-        # Convert frame to grayscale for better accuracy
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         
         # Detect faces in the frame
